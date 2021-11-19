ab8615536142350b46ecdb0ee9184afca66bc3e7
Y-anlah Sophia Lopez <92627335+yanlahlopez@users.noreply.github.com>
2021-11-19 21:24:52 +0800

    Delete apple.py

--- apple.py
+++
@@ -1,7 +0,0 @@
-money = int(input("Amount of money you have: "))
-print("An apple is 20 pesos.")
-apple = 20
-appletotal = money // apple
-total = appletotal * apple
-change = money - total
-print(f"You can buy {appletotal} apples and your change is {change} pesos.")
