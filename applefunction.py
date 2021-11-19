1ea07555b9fe513ca1fc53aa392007705731e34d
Y-anlah Sophia Lopez <92627335+yanlahlopez@users.noreply.github.com>
2021-11-19 21:25:01 +0800

    Delete applefunction.py

--- applefunction.py
+++
@@ -1,9 +0,0 @@
-import math
-money = float(input("Amount of money you have: "))
-print("An apple is 20 pesos.")
-apple = 20
-appletotal = money // apple
-appletotal1 = math.floor(appletotal)
-total = appletotal * apple
-change = money - total
-print(f"You can buy {appletotal1} apples and your change is {change:.2f} pesos.")
